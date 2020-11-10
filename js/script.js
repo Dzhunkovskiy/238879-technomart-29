b0d3fb55b27df64b241e48c0f2cbb99242ced42d
Dzhunkovskiy <62470046+Dzhunkovskiy@users.noreply.github.com>
2020-11-11 02:51:55 +0300

    последняя подготовка к защите

--- js/script.js
+++ js/script.js
@@ -6,8 +6,54 @@ const feedbackClose = document.querySelector(".feedback-close");
 const mapClose = document.querySelector(".map-close");
 const modalCart = document.querySelector(".modal-cart-wrapper");
 const closeCart = document.querySelector(".close-cart-modal");
-const buyButton = document.querySelector(".buy-button");
+const buyButton = document.querySelectorAll(".buy-button");
+const deliveryButton = document.querySelector(".delivery-slide-button");
+const warrantyButton = document.querySelector(".warranty-slide-button");
+const creditButton = document.querySelector(".credit-slide-button");
+const slideDelivery = document.querySelector(".service-description-delivery");
+const slideWarranty = document.querySelector(".service-description-warranty");
+const slideCredit = document.querySelector(".service-description-credit");
 
+
+if (buyButton && buyButton.length) {
+  for(let i = 0; i < buyButton.length; i++) {
+    buyButton[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      modalCart.classList.add("modal-show");
+    })
+  }
+};
+
+closeCart.addEventListener("click", function (evt) {
+  modalCart.classList.remove("modal-show");
+});
+
+deliveryButton.addEventListener("click", function (evt) {
+  slideDelivery.classList.remove("slide-hidden");
+  slideWarranty.classList.add("slide-hidden");
+  slideCredit.classList.add("slide-hidden");
+  deliveryButton.classList.add("services-item-selected");
+  warrantyButton.classList.remove("services-item-selected");
+  creditButton.classList.remove("services-item-selected");
+});
+
+warrantyButton.addEventListener("click", function (evt) {
+  slideDelivery.classList.add("slide-hidden");
+  slideWarranty.classList.remove("slide-hidden");
+  slideCredit.classList.add("slide-hidden");
+  deliveryButton.classList.remove("services-item-selected");
+  warrantyButton.classList.add("services-item-selected");
+  creditButton.classList.remove("services-item-selected");
+});
+
+creditButton.addEventListener("click", function (evt) {
+  slideDelivery.classList.add("slide-hidden");
+  slideWarranty.classList.add("slide-hidden");
+  slideCredit.classList.remove("slide-hidden");
+  deliveryButton.classList.remove("services-item-selected");
+  warrantyButton.classList.remove("services-item-selected");
+  creditButton.classList.add("services-item-selected");
+});
 
 lostButton.addEventListener("click", function (evt) {
 	modalFeedback.classList.add("modal-show");
@@ -31,10 +77,3 @@ mapClose.addEventListener("click", function (evt) {
 	modalMap.classList.remove("modal-show");
 });
 
-buyButton.addEventListener("click", function (evt) {
-	modalCart.classList.add("modal-show");
-});
-
-closeCart.addEventListener("click", function (evt) {
-	modalCart.classList.remove("modal-show");
-});
